ebf1cba4c094e57d01896fa4c10520537fbf2257
Your Name <sivadhanushuppalapati@gmail.com>
2025-11-30 18:05:50 +0530

    1.0 commit config email

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,7 +113,7 @@ export default function RootLayout({
         {/* Font Awesome */}
         {/* <link 
           rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+          href="https://cdnjs. cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
           crossOrigin="anonymous"
         /> */}
         
